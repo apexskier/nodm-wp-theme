098ca5aa5a52b9455f6a413ce1c3d2a8d3cbeb70
Cameron Little <cameron@camlittle.com>
2016-01-25 03:31:56 +0000

    Fixed countdown being one month late.

--- front-page.php
+++ front-page.php
@@ -16,18 +16,18 @@
             if (dateMatch) {
                 var month = (function() {
                     switch (dateMatch[1].toLowerCase()) {
-                        case 'june': return 6; // common case
-                        case 'january': return 1;
-                        case 'february': return 2;
-                        case 'march': return 3;
-                        case 'april': return 4;
-                        case 'may': return 5;
-                        case 'july': return 7;
-                        case 'august': return 8;
-                        case 'september': return 9;
-                        case 'october': return 10;
-                        case 'november': return 11;
-                        case 'december': return 12;
+                        case 'june': return 5; // common case
+                        case 'january': return 0;
+                        case 'february': return 1;
+                        case 'march': return 2;
+                        case 'april': return 3;
+                        case 'may': return 4;
+                        case 'july': return 6;
+                        case 'august': return 7;
+                        case 'september': return 8;
+                        case 'october': return 9;
+                        case 'november': return 10;
+                        case 'december': return 11;
                     };
                 })();
                 var day = dateMatch[2];
